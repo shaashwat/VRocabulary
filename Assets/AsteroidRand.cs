8d89144964c5abe4c2ff0009dcf079f3a0bbf43b
shaashwat <shaash317@gmail.com>
2016-11-12 14:59:39 -0500

    initial commit

---
+++ Assets/AsteroidRand.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidRand : MonoBehaviour 
+{
+    
+    // Instantiate the prefab somewhere between -10.0 and 10.0 on the x-z plane 
+    void Start() 
+    {
+    	float x = Random.Range(-10.0f, 10.0f);
+    	float z = Random.Range(-10.0f, 10.0f);
+    	transform.position = new Vector3(x,0,z);
+    }   
+}
